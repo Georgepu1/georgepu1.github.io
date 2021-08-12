b4e9a4c04efa1ebfd5830a4113397af220f1454d
George Pu <georgewpu@gmail.com>
2021-08-11 22:49:42 -0700

    Fixing text sizing and added bullet points

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,12 +5,30 @@ class Header extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var description= this.props.data.description;
       var resumeDownload = this.props.data.resumedownload;
       var networks= this.props.data.social.map(function(network){
-        return <li style={{padding: '10px'}} key={network.name}><a target="_blank" href={network.url}><i className={network.className}></i></a></li>
+        return <li style={{padding: '10px'}} key={network.name}><a target="_blank" rel="noopener noreferrer" href={network.url}><i className={network.className}></i></a></li>
       })
     }
+
+    var descriptions = [
+      {
+         name: '- I\'m an entering (Fall 21) first year graduate CS student at UCLA,',
+         key: '0',
+      },
+      {
+         name: '- I\'m actively doing research in NLP under Prof. Jingbo Shang (UCSD),',
+         key: '1',
+      },
+      {
+         name: '- I\'m a SWE Intern at Convoy and have worked at AMEX/other startups, and ',
+         key: '2',
+      },
+      {
+         name: '- I\'m into gyming, hiking, sports (tennis/table tennis) and gaming (chess/valo)', 
+         key: '3'
+      }
+    ];
 
     return (
       <header id="home">
@@ -30,12 +48,19 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">Hey, I'm {name}.</h1>
-            <h3>{description}</h3>
+            <h1 className="responsive-headline">
+               <div>Hey! I'm {name}. Currently,</div>
+            </h1>
+            <h3>
+               <ul>
+                 { descriptions.map(desc => <li key={desc.key}>{desc.name}</li> )}
+               </ul>
+            </h3>
+            <div>Feel free to reach me at gnpu@g.ucla.edu</div>
             <hr />
             <ul className="social">
                {networks}
-               <a style={{margin: '10px'}} href={resumeDownload} target="_blank" className="button"><i className="fa fa-download"></i> Resume</a>
+               <a style={{margin: '10px'}} href={resumeDownload} target="_blank" rel="noopener noreferrer" className="button"><i className="fa fa-download"></i> Resume</a>
             </ul>
          </div>
       </div>
