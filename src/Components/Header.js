5fbe127dc081a2c48248795428ad5213e27acf8d
George Pu <georgewpu@gmail.com>
2022-04-12 00:35:10 -0700

    updated header information for current research interests

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,14 +30,16 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">
-               <div>Hey! I'm {name}, and </div>
+               <div>Hey! I'm {name}, and I'm</div>
             </h1>
             <h3>
                <ul>
-                  <li>- I'm a first year graduate CS student at <a href="https://www.ucla.edu/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>UCLA</a> 📚</li>
-                  <li>- I'm a (returning) SWE Intern at <a href="https://convoy.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Convoy</a>, <a href="https://www.americanexpress.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>AMEX</a>, and incubator startups 🧑🏻‍💻</li>
-                  <li>- I'm actively doing NLP research under <a href="https://shangjingbo1226.github.io/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Prof. Jingbo Shang (UCSD)</a>📄</li>
-                  <li>- I'm into gyming, traveling/hiking, and sports (tennis/table tennis) 🏃</li>
+                  <li>- a first year graduate CS student at <a href="https://www.ucla.edu/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>UCLA</a></li>
+                  <li>- interested in intelligent information extraction, organization, generation, and delivery</li>
+                  <li>- working on utility patents and tech associated with "Interactive Information Capture and Retrieval", intelligent navigation, and search optimization</li>
+                  <li>- actively doing ML/DL/NLP research (Past: <a href="https://shangjingbo1226.github.io/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Prof. Jingbo Shang @ UCSD</a>)</li>
+                  <li>- a SWE Intern at <a href="https://convoy.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Convoy </a> (returning), <a href="https://www.americanexpress.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>AMEX</a>, and incubator startups</li>
+                  <li>- into gyming, traveling/hiking, and sports (tennis/table tennis) 🏃</li>
                </ul>
             </h3>
             <div>Feel free to reach me at gnpu@cs.ucla.edu</div>
