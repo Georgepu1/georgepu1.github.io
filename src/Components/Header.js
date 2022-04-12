2f613b3788e36166952034f939a78bdc0dcdb79f
George Pu <georgewpu@gmail.com>
2022-04-12 13:18:34 -0700

    changed header into a paragraph

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,19 +30,25 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">
-               <div>Hey! I'm {name}, and I'm</div>
+               <div>Hey! I'm {name}</div>
             </h1>
             <h3>
                <ul>
-                  <li>- a first year graduate CS student at <a href="https://www.ucla.edu/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>UCLA</a></li>
-                  <li>- interested in intelligent information extraction, organization, generation, and delivery</li>
-                  <li>- working on utility patents and tech associated with "Interactive Information Capture and Retrieval", intelligent navigation, and search optimization</li>
-                  <li>- actively doing ML/DL/NLP research (Past: <a href="https://shangjingbo1226.github.io/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Prof. Jingbo Shang @ UCSD</a>)</li>
+                  I'm a first year graduate CS student at <a href="https://www.ucla.edu/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>UCLA</a>. 
+                      My technical interests are in  intelligent information extraction, organization, generation, and delivery. 
+                      I've co-authored a patent application on prompt-based and interactive info capture and retrieval (with ML/DL/NLP/CV techniques), and I am working on other patent/technical areas for intelligent navigation and search result optimization. 
+                      Along with this, I'm actively researching ML/DL/NLP (Past: <a href="https://shangjingbo1226.github.io/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Prof. Jingbo Shang @ UCSD</a>). 
+                      I've been a SWE Intern at <a href="https://convoy.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Convoy </a> (2x), <a href="https://www.americanexpress.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>AMEX</a>, and incubator startups.
+                      Lastly, I'm into gyming, traveling/hiking, and sports (tennis/table tennis).
+                  
+                  {/* <li>- interested in intelligent information extraction, organization, generation, and delivery</li> */}
+                  {/* <li>- working on utility patents and tech associated with "Interactive Information Capture and Retrieval", intelligent navigation, and search optimization</li>
+                  <li>- actively researching ML/DL/NLP (Past: <a href="https://shangjingbo1226.github.io/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Prof. Jingbo Shang @ UCSD</a>)</li>
                   <li>- a SWE Intern at <a href="https://convoy.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>Convoy </a> (returning), <a href="https://www.americanexpress.com/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: "bold" }}>AMEX</a>, and incubator startups</li>
-                  <li>- into gyming, traveling/hiking, and sports (tennis/table tennis) 🏃</li>
+                  <li>- into gyming, traveling/hiking, and sports (tennis/table tennis) 🏃</li> */}
                </ul>
             </h3>
-            <div>Feel free to reach me at gnpu@cs.ucla.edu</div>
+            <div>Feel free to reach me at gnpu@g.ucla.edu</div>
             <hr />
             <ul className="social">
                {networks}
